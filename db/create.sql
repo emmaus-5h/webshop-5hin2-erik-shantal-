ca8ea60c2786642a020acfec89c5d8a27c01fafc
ErikKleijn <115073471+ErikKleijn@users.noreply.github.com>
2024-01-30 08:31:52 +0000

    feebdack GEE

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,11 @@
 -- create tables
 --
 
+-- feedback GEE 30 jan 2024
+-- de tabellen voor het merk heb je goed gemaakt
+-- let op de eisen die onder beoordelig staan in de opdracht
+-- daarin staat dat een n:m ook nodig is
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -9,11 +14,18 @@ CREATE TABLE products (
   description TEXT,
   price NUMERIC(10, 2),
   merk_id INTEGER
+  kleur varchar (255)
 );
 CREATE TABLE merk (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR (255)
   );
+
+CREATE TABLE kleur
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR (255)
+  );
+  
   
 
 --
@@ -25,10 +37,10 @@ CREATE TABLE merk (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, merk_id) values ('White Stussy T-shirt', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5, 1);
-insert into products (name, description, code, price, merk_id) values ('Black Stussy Hoodie', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3' 11, 1);
-insert into products (name, description, code, price, merk_id) values ('Black Stussy T-shirt', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5, 1);
-insert into products (name, description, code, price, merk_id) values ('White Stussy Hoodie', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5, 1);
+insert into products (name, description, code, price, merk_id, kleur) values ('White Stussy T-shirt', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5, 1, 'wit'););
+insert into products (name, description, code, price, merk_id) values ('Black Stussy Hoodie', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3' 11, 1), 'zwart';
+insert into products (name, description, code, price, merk_id) values ('Black Stussy T-shirt', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5, 1, 'zwart'););
+insert into products (name, description, code, price, merk_id) values ('White Stussy Hoodie', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5, 1, 'wit'););');
 insert into products (name, description, code, price, merk_id) values ('Beige Blur Stussy T-shirt', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14, 1);
 insert into products (name, description, code, price, merk_id) values ('Blue Blur Stussy T-shirt', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 1);
 
